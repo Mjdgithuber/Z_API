aec69936a19fc2c8ee9686e6ceaa50f02c3beb1e
Mjdgithuber <matthewjdennerlein@gmail.com>
2021-02-24 15:50:36 -0500

    First test of altered lz4

--- src/main.c
+++ src/main.c
@@ -1,3 +1,9 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "lz4.h"
 
 typedef unsigned char BYTE;
 
@@ -25,21 +31,22 @@ void compress_block(BYTE* src, BYTE* dest, unsigned dest_size, header* h, unsign
 }
 
 void decompress_block(BYTE* src, BYTE* dest) {
-	unsigned i;
+	unsigned i, result;
 
 	header* h = (header*) src;
 	LZ4_streamDecode_t* const lz4_sd = LZ4_createStreamDecode();
 	BYTE *c_src = src + sizeof(header);
 	BYTE *c_dest = dest;
 	for(i = 0; i < h->units; i++) {
-		LZ4_decompress_safe_continue(lz4_sd, c_src, c_dest, 1000, h->unit_size);
-		
+		printf("Decompressing unit %u\n", i);
+		/*result = LZ4_decompress_safe_continue(lz4_sd, c_src, c_dest, 1000, h->unit_size);*/
+		result = LZ4_decompress_safe_continue_unkown_size (lz4_sd, c_src, c_dest, h->unit_size, 512);
+		printf("Decompressed unit %u with compressed size of result %u\n", i, result);
+		break;
 	}
 
 	LZ4_freeStreamDecode(lz4_sd);
 }
-
-int LZ4_decompress_safe_continue (LZ4_streamDecode_t* LZ4_streamDecode, const char* source, char* dest, int compressedSize, int maxOutputSize)
 
 void generate_block(BYTE* src, short int unit_size, short int units, BYTE* dest, unsigned dest_size, unsigned* size) {
 	
@@ -51,110 +58,35 @@ void generate_block(BYTE* src, short int unit_size, short int units, BYTE* dest,
 	(*size) += sizeof(header);
 }
 
-/* return status code */
-/*int modify(BYTE* existing_compressed_block, BYTE* src, unsigned offset, unsigned size, BYTE* output_buffer, unsigned* output_size) {
+int main() {
+	FILE* fp;
+	BYTE in[256];
+	BYTE out[256*2];	
+
+	fp = fopen("test_compress.txt", "r");
 	
-}*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/* this is calloc'ed */
-typedef struct {
-	BYTE meta;
-	BYTE stored_blocks;
-	BYTE* c_buff;
-	BYTE* uc_buff; /* will be used later to store uncompressed maybe first 4 blocks */
-} slab_header;
-
-typedef struct {
-	BYTE** mapping;
-	struct slab_header* header;
-	int block_size; /* size of block in bytes */
-	int slab_size; /* size of slab in bytes */
-	int slabs;  /* number of slabs */
-	int blocks_per_slab; /* number of blocks in the slab based on block_size and slab_size */
-} partition;
-
-BYTE* get_decompression_buffer(partition* par) {
-	return malloc(par->block_size * par->slab_size);
-}
-
-void decompress(BYTE* compressed_buffer, ) {
-
-}
-
-void store_block(int lba, partition* par, void* block) {
-	int slab = lba / par->blocks_per_slab;
-	int offset = lba % par->blocks_per_slab;
-
-	slab_header* header = &par->header[slab];
-	BYTE* buf = get_decompression_buffer(par);	
-	if(stored_blocks) {
-		decompress(header->c_buff, buf, par->blocks_per_slab);
-	}
+	int num = fread(in, 1, 256, fp);
+	printf("Read %d chars from input!\n", num);
 	
-	// clear (zero out) garbage between last block and current block
-	if(stored_blocks < offset)
-		memset(buf + (stored_blocks-1 * par->block_size), 0, (offset - stored_blocks) * par->block_size);
-	memcpy(buf + (par->block_size * offset), block, par->block_size); // copy block into buf
+	int size;
+	generate_block(in, 64, 4, out, 256*2, &size);
+
+	printf("Output compressed size %d!\n", size);
+
+	printf("\nTesting output block:\n");
+	printf(" > Header:\n");
+	printf("   > Unit Size: %u\n", ((header*)out)->unit_size);
+	printf("   > Units    : %u\n", ((header*)out)->units);
+
+	printf("\nOrginal buffer: '%s'\n", in);
 	
-	// re-compress buffer
-	header->stored_blocks = offset+1;
+	printf("\nCleared buffer: ");
+	memset(in, 0, 256);
+	printf("'%s'\n", in);	
 
+	decompress_block(out, in);
+	printf("\nUncomp  buffer: '%s'\n", in);
 
+	fclose(fp);
+	return 0;
 }
-
-void get_block(int lba, partition* par, void* block_store) {
-	int slab = lba / par->blocks_per_slab;
-
-	if(par->header[slab].valid) {
-		// check if compressed
-		// if so uncompress
-		// also check if block exists
-	}
-
-	// else return all zeros
-}
-
-partition* make_partition(int block_size, int slab_size, int slabs) {
-	partition* par;
-
-	assert(!(slab_size % block_size));
-
-	par = malloc(sizeof(partition));
-	par->block_size = block_size;
-	par->slab_size = slab_size;
-	par->slabs = slabs;
-	par->blocks_per_slab = slab_size / block_size;
-	
-	par->header = calloc(slabs, sizeof(slab_header));
-	par->mapping = malloc(sizeof(byte*) * slabs);
-	
-	return par;
-}
-
-void free_partition(partition* par) {
-	int i;
-
-	for(i = 0; i < par->slabs; i++) {
-		if(par->header[i].valid)
-			free(&par->mapping[i]);
-	}
-	free(par->header);
-	free(par->mapping);
-	free(par);
-}
-
